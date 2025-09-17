8369d2503bb4ce127fc0aa9c60976a4378c8adfe
Eshaan Vaswani <eshaanv2004@gmail.com>
2025-09-17 17:50:34 +0530

    feat: widget sessions and auth screen

--- apps/widget/modules/widget/ui/components/widget-footer.tsx
+++ apps/widget/modules/widget/ui/components/widget-footer.tsx
@@ -28,7 +28,7 @@ export const WidgetFooter = () => {
             variant="ghost"
          >
             <InboxIcon
-               className={cn("size-5", screen === "inbox" && "text-primary")}
+               className={cn("size-5")}
             />
          </Button>
       </footer>
